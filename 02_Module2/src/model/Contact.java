5cbb280630a3ff25bd2e90fde043a01034bf9c27
Thuan Nguyen <tnguyen@ilts.com>
2024-08-29 19:39:15 +0700

    step

---
+++ 02_Module2/src/model/Contact.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Contact {
+    private int phoneNum;
+    private String group;
+    private String name;
+    private String address;
+    private String gender;
+    private LocalDate birthday;
+    private String email;
+
+
+}
--- 02_Module2/src/view/MainMenu.java
+++ 02_Module2/src/view/MainMenu.java
@@ -1,10 +1,29 @@
 package view;
 
 
+import input.Input;
 
 public class MainMenu {
     public void showMenu(){
-        System.out.println("========== Menu ==========");
+        int choice;
+
+
+        do {
+            System.out.println("========== Chương trình quản lý danh bạ ==========");
+            System.out.println("1. Xem danh sách");
+            System.out.println("2. Them mới");
+            System.out.println("3. Cập nhật");
+            System.out.println("4. Xóa");
+            System.out.println("5. Tìm kiếm");
+            System.out.println("6. Đọc từ file");
+            System.out.println("7. Ghi vào file");
+            System.out.println("8. Thoát");
+
+            System.out.print("Chọn chức năng: ");
+            choice = Input.inputNumber();
+            System.out.println();
+        } while(choice != 8);
+
     }
 
 
