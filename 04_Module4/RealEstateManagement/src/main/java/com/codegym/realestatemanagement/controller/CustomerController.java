280ea2f693d708abf20ace7f129dbc52692f19a1
Thuan Nguyen <tnguyen@ilts.com>
2024-12-24 23:06:49 +0700

    TransactionController

---
+++ 04_Module4/RealEstateManagement/src/main/java/com/codegym/realestatemanagement/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.codegym.realestatemanagement.controller;
+
+import com.codegym.realestatemanagement.model.Customer;
+import com.codegym.realestatemanagement.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
---
+++ 04_Module4/RealEstateManagement/src/main/java/com/codegym/realestatemanagement/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package com.codegym.realestatemanagement.controller;
+import com.codegym.realestatemanagement.model.Customer;
+import com.codegym.realestatemanagement.model.Product;
+import com.codegym.realestatemanagement.model.Transaction;
+import com.codegym.realestatemanagement.service.IProductService;
+import com.codegym.realestatemanagement.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    ITransactionService transactionService;
+
+    @GetMapping("")
+    public ModelAndView listProducts() {
+        Iterable<Transaction> transaction = transactionService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/transaction/list");
+        modelAndView.addObject("transaction", transaction);
+        return modelAndView;
+    }
+}
